5d95e95fe839571252ce353bfca6b5a837bea1a5
TakCastel <takcastel@gmail.com>
2025-08-13 14:42:44 +0200

    correct langdir path

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -41,7 +41,7 @@ export default defineNuxtConfig({
   },
   i18n: {
     defaultLocale: 'fr',
-    langDir: 'i18n/locales/',
+    langDir: '/i18n/locales/',
     locales: [
       { code: 'fr', name: 'Français', file: 'fr.json' },
       { code: 'en', name: 'English', file: 'en.json' }
